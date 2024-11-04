a56428f3abfea96c43a120c33b0fdb35742efc58
ray-03 <rinaldy.richard.03@gmail.com>
2024-11-04 08:52:47 +0700

    feat: add header

--- app/App.tsx
+++ app/App.tsx
@@ -1,65 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import {
-  createTheme,
-  darkColors,
-  lightColors,
-  ThemeProvider,
-} from '@rneui/themed';
-import React, {type PropsWithChildren} from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  Linking,
-  StyleProp,
-  TextStyle,
-  ViewStyle,
-  Platform,
-  useColorScheme,
-} from 'react-native';
-import {Header, HeaderProps, Icon} from '@rneui/themed';
-// import {TouchableOpacity} from 'react-native-gesture-handler';
+import React from 'react';
+import {View, useColorScheme, TouchableOpacity, ScrollView} from 'react-native';
+import {Header, Icon, SearchBar} from '@rneui/themed';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {ThemeProvider} from '@rneui/themed';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import {customTheme} from './style/theme';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -68,93 +13,34 @@ const App = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  const theme = createTheme({
-    lightColors: {
-      ...Platform.select({
-        default: lightColors.platform.android,
-        ios: lightColors.platform.ios,
-      }),
-    },
-    darkColors: {
-      ...Platform.select({
-        default: darkColors.platform.android,
-        ios: darkColors.platform.ios,
-      }),
-    },
-  });
-
   return (
-    <ThemeProvider theme={theme}>
-      <SafeAreaProvider>
+    <ThemeProvider theme={customTheme}>
+      <SafeAreaProvider style={backgroundStyle}>
         <Header
-          leftComponent={{
-            icon: 'menu',
-            color: '#fff',
-          }}
+          placement="left"
+          centerComponent={
+            <SearchBar
+              containerStyle={{
+                flexDirection: 'row',
+              }}
+            />
+          }
           rightComponent={
-            <View>
-              {/* <TouchableOpacity
-              //  onPress={docsNavigate}
-              >
-                <Icon name="description" color="white" />
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={{marginLeft: 10}}
-                // onPress={playgroundNavigate}
-              >
-                <Icon type="antdesign" name="rocket1" color="white" />
-              </TouchableOpacity> */}
-            </View>
+            <TouchableOpacity
+            //  onPress={shopCartNavigate}
+            >
+              <Icon name="shopping-cart" />
+            </TouchableOpacity>
           }
         />
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={backgroundStyle}>
+          <View></View>
+        </ScrollView>
       </SafeAreaProvider>
     </ThemeProvider>
-
-    // <SafeAreaView style={backgroundStyle}>
-    //   <StatusBar
-    //     barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-    //     backgroundColor={backgroundStyle.backgroundColor}
-    //   />
-    //   <ScrollView
-    //     contentInsetAdjustmentBehavior="automatic"
-    //     style={backgroundStyle}>
-    //     {/* <Header /> */}
-    //     <View
-    //       style={{
-    //         backgroundColor: isDarkMode ? Colors.black : Colors.white,
-    //       }}>
-    //       <Section title="Step One">
-    //         Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-    //         screen and then come back to see your edits.
-    //       </Section>
-    //       <Section title="See Your Changes"></Section>
-    //       <Section title="Debug"></Section>
-    //       <Section title="Learn More">
-    //         Read the docs to discover what to do next:
-    //       </Section>
-    //     </View>
-    //   </ScrollView>
-    // </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
